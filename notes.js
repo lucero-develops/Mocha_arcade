97dfb23c89dc6dc98a68402b050ab23f0b682974
lucero-develops <mike.j.lucero@gmail.com>
2017-02-07 19:30:00 -0500

    constructor prototypes

--- notes.js
+++ notes.js
@@ -2,51 +2,42 @@
 Learn to use the .prototype key to simplify thinking
 about objects and improve your applications' performance
 */
+/*
+previously where where coping all the methods from 
+Car.methods over to the new instance.
+Now we are delegating failed lookups to Car.methods
+*/
+/*
+The steps for making a class in this prottypal pattern
+are pretty clear 
+-You need a function that allow you to make instances 
+-a line in that function that generates the
+ 	new instance object
+-a delagation from the new object to some prototype object
+-and some logic for augmenting the object
+	with properties that make it unique from all the other 
+	objects of the same class.
+
+SINCE BUILDING A HOLDER OBJECT FOR METHODS 
+AND ATTACHING IT AS A PROPERTY TO THE CONSTRUCTOR FUNCTION
+IS SO COMMON THE LANGUAGE DOES THIS FOR YOU AUTOMATICALLY
+
+WHEN EVER A FUNCTION IS CREATED IT WILL HAVE AN OBJECT 
+ATTACHED TO IT THAT YOU CAN USE AS A CONTAINER OF METHODS 
+JUST INCASE YOU PLAN ON USING THAT FUNCTION TO BUILD 
+INSTANCES OF A CLASS
+
+
+THIS IS STORED AT THE KEY ".prototype"
+
+*/
 
 
 Var Car = function(loc){
-	var obj ={loc:loc};
-	//extend is usually a high level function that copies 
-	//the prototype of a new subclass that you want to extend
-	// from the base class.
-	extend(obj, Car.methods);
-	return obj;
-};
-Car.methods = {
-	move : function(){
-		this.loc++;
-	}
-}
-
-//Can you think of a way to inprove performance
-// using prototypes proto type chains ?
-
-// 
-Var Car = function(loc){
-	/*var obj ={loc:loc};
-	problem here is where using an 
-	object literal in order to build 
-	the brand new instance of car and
-	WHEN YOU USE AN OBJECT LITERAL to define
-	an object you CANT SET ITS PROTOTYPE 
-	SO>>>>>> use your other option to create 
-	objects which is Object.create(This is going to be the thing its 
-	delegates to)
-	*/
-
-	var obj = Object.create(Car.methods);
-	//failed property lookups on these new instances 
-	//will fall threw to Car.methods now
-
-	//we now need to insure the loc property gets
-	//added to our new object , since its no longer
-	//inline as an object literal 
+	var obj = Object.create(Car.prototype);
 	obj.loc = loc;
-
 	return obj ;
 };
-Car.methods={
-	move : function(){
+Car.prototype.move = function(){
 		this.loc++
-	}
-}
+};
